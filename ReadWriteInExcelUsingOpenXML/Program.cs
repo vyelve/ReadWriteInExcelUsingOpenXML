14f1a6f12ee3d0e999774e4cd938e11348e1e853
Vishal Yelve <vishal_yelve@hotmail.com>
2020-04-23 21:36:30 +0530

    Add files via upload

---
+++ ReadWriteInExcelUsingOpenXML/Program.cs
@@ -0,0 +1,152 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ReadWriteInExcelUsingOpenXML
+{
+    public partial class Program
+    {             
+        static void Main(string[] args)
+        {
+            try
+            {
+                var dataTable = new DataTable();
+                dataTable.Columns.Add("File Name", typeof(String));
+                dataTable.Columns.Add("File Path", typeof(String));
+
+                dataTable.Rows.Add("testing_123.txt", "F:\\Study Materials\\Projects\\Practice");
+                dataTable.Rows.Add("testing_file1111.txt", "F:\\Study Materials\\Projects\\Practice");
+
+                var _dataTable = GetExcelDatatoDataTable();
+                if (_dataTable.Rows.Count > 0)
+                {
+                    _dataTable.Merge(dataTable);
+                    WriteExcelFile(_dataTable, @"F:\Study Materials\Projects\Practice\Report.xlsx"); // merge datatable data
+                }
+                else
+                {
+                    WriteExcelFile(dataTable, @"F:\Study Materials\Projects\Practice\Report.xlsx");// new datatable data
+                }
+
+                Console.WriteLine("Data inserted !!");
+                
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error occure while processing");
+                Console.ReadKey();
+            }
+            
+            
+
+        }
+
+        public static string GetCellValue(SpreadsheetDocument document, Cell cell)
+        {
+            SharedStringTablePart stringTablePart = document.WorkbookPart.SharedStringTablePart;
+            string value = cell.CellValue.InnerXml;
+
+            if (cell.DataType != null && cell.DataType.Value == CellValues.SharedString)
+            {
+                return stringTablePart.SharedStringTable.ChildElements[Int32.Parse(value)].InnerText;
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+        private static void WriteExcelFile(DataTable table, string outputPath)
+        {
+            using (SpreadsheetDocument document = SpreadsheetDocument.Create(outputPath, SpreadsheetDocumentType.Workbook))
+            {
+                WorkbookPart workbookPart = document.AddWorkbookPart();
+                workbookPart.Workbook = new Workbook();
+
+                WorksheetPart worksheetPart = workbookPart.AddNewPart<WorksheetPart>();
+                var sheetData = new SheetData();
+                worksheetPart.Worksheet = new Worksheet(sheetData);
+
+                Sheets sheets = workbookPart.Workbook.AppendChild(new Sheets());
+                Sheet sheet = new Sheet() { Id = workbookPart.GetIdOfPart(worksheetPart), SheetId = 1, Name = "mySheet" };
+
+                sheets.Append(sheet);
+
+                Row headerRow = new Row();
+
+                List<String> columns = new List<string>();
+                foreach (System.Data.DataColumn column in table.Columns)
+                {
+                    columns.Add(column.ColumnName);
+
+                    Cell cell = new Cell();
+                    cell.DataType = CellValues.String;
+                    cell.CellValue = new CellValue(column.ColumnName);
+                    headerRow.AppendChild(cell);
+                }
+
+                sheetData.AppendChild(headerRow);
+
+                foreach (DataRow dsrow in table.Rows)
+                {
+                    Row newRow = new Row();
+                    foreach (String col in columns)
+                    {
+                        Cell cell = new Cell();
+                        cell.DataType = CellValues.String;
+                        cell.CellValue = new CellValue(dsrow[col].ToString());
+                        newRow.AppendChild(cell);
+                    }
+
+                    sheetData.AppendChild(newRow);
+                }
+
+                workbookPart.Workbook.Save();
+            }
+        }
+
+        private static DataTable GetExcelDatatoDataTable()
+        {
+            var table = new DataTable();
+            using (SpreadsheetDocument spreadSheetDocument = SpreadsheetDocument.Open(@"F:\Study Materials\Projects\Practice\Report.xlsx", false))
+            {
+                WorkbookPart workbookPart = spreadSheetDocument.WorkbookPart;
+                IEnumerable<Sheet> sheets = spreadSheetDocument.WorkbookPart.Workbook.GetFirstChild<Sheets>().Elements<Sheet>();
+                string relationshipId = sheets.First().Id.Value;
+                WorksheetPart worksheetPart = (WorksheetPart)spreadSheetDocument.WorkbookPart.GetPartById(relationshipId);
+                Worksheet workSheet = worksheetPart.Worksheet;
+                SheetData sheetData = workSheet.GetFirstChild<SheetData>();
+                IEnumerable<Row> rows = sheetData.Descendants<Row>();
+
+                foreach (Cell cell in rows.ElementAt(0))
+                {
+                    table.Columns.Add(GetCellValue(spreadSheetDocument, cell));
+                }
+
+                foreach (Row row in rows) //this will also include your header row...
+                {
+                    DataRow tempRow = table.NewRow();
+
+                    for (int i = 0; i < row.Descendants<Cell>().Count(); i++)
+                    {
+                        tempRow[i] = GetCellValue(spreadSheetDocument, row.Descendants<Cell>().ElementAt(i));
+                    }
+                    
+                    table.Rows.Add(tempRow);
+                }
+            }
+
+            table.Rows.RemoveAt(0);                        
+            
+            return table;
+        }    
+                
+    }
+}
